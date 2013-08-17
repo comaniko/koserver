fabb98e97d3c93ec6dbcbbb68960e30df180a3f6
Eridia <eridiaa@gmail.com>
2013-08-18 02:47:24 +0300

    Clana otomatik bağış sistemi orjinal sisteme göre yapılandırıldı.Artık gelen np'nin 8'de 1'i clana bağışlanır.

--- src/GameServer/User.cpp
+++ src/GameServer/User.cpp
@@ -613,21 +613,7 @@ void CUser::SendLoyaltyChange(int32 nChangeAmount /*= 0*/, bool bIsKillReward /*
 			if (pKnights != nullptr) {
 				if (pKnights->m_byFlag >= ClanTypeAccredited5)
 				{
-					if (pKnights->m_sMembers <= 5)
-						nClanLoyaltyAmount = 1;
-					else if (pKnights->m_sMembers <= 10)
-						nClanLoyaltyAmount = 2;
-					else if (pKnights->m_sMembers <= 15)
-						nClanLoyaltyAmount = 3;
-					else if (pKnights->m_sMembers <= 20)
-						nClanLoyaltyAmount = 4;
-					else if (pKnights->m_sMembers <= 25)
-						nClanLoyaltyAmount = 5;
-					else if (pKnights->m_sMembers <= 30)
-						nClanLoyaltyAmount = 6;
-					else if (pKnights->m_sMembers > 30)
-						nClanLoyaltyAmount = 6;
-
+					nClanLoyaltyAmount = m_iLoyalty / 8;
 					m_iLoyalty -= nClanLoyaltyAmount;
 					CKnightsManager::AddUserDonatedNP(GetClanID(),m_strUserID,nClanLoyaltyAmount,true);
 				}
